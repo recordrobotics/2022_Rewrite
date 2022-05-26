190a431a75918d6f23f13c3de89f844c273fb2d0
Vladyslav Aviedov <vladaviedov@protonmail.com>
2022-05-26 14:19:34 -0400

    minor tweaking in autoshoot

--- src/main/java/org/recordrobotics/munchkin/commands/auto/AutoShoot.java
+++ src/main/java/org/recordrobotics/munchkin/commands/auto/AutoShoot.java
@@ -6,11 +6,15 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class AutoShoot extends CommandBase {
+
+	// Delay to allow the flywheel to spin up
+	private static final double SHOOT_DELAY = 0.8;
+	// Time needed to execute the command
+	private static final double END_TIME = 2.0;
+
 	private Flywheel _flywheel;
 	private double _speed;
 	private Timer _shootTimer = new Timer();
-	private static final double START_UP_TIME = 2.0; // the time, in , it takes to start spinning the flywheel
-	private static final double SHOOT_TIME = 5.0; // the total time the flywheel will be running (start up time + time it takes to shoot the ball)
 
 	public AutoShoot(Flywheel flywheel, double speed) {
 		if (speed <= 0) {
@@ -30,16 +34,22 @@ public class AutoShoot extends CommandBase {
 		_flywheel.spin(_speed);
 	}
 
+	/**
+	 * Shoot after delay
+	 */
 	@Override
 	public void execute() {
-		if (_shootTimer.get() > START_UP_TIME) {
+		if (_shootTimer.get() > SHOOT_DELAY) {
 			_flywheel.shootServos();
 		}
 	}
 
+	/**
+	 * Command ends when time runs out
+	 */
 	@Override
 	public boolean isFinished() {
-		return _shootTimer.get() >= SHOOT_TIME;
+		return _shootTimer.get() >= END_TIME;
 	}
 	/**
 	 * Stops the flywheel and resets the servos once finished

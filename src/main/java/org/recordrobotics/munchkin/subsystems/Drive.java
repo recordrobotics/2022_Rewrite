e7bb27107c287867f57a85519e91173925dabf45
Rit Garg <rit.g.garg@gmail.com>
2023-01-05 20:24:08 -0500

    fixing negatives in drive

--- src/main/java/org/recordrobotics/munchkin/subsystems/Drive.java
+++ src/main/java/org/recordrobotics/munchkin/subsystems/Drive.java
@@ -48,8 +48,8 @@ public class Drive extends SubsystemBase {
 		// Arcade drive expects rotational inputs, while get translational
 		// inputs. Therefore the values must be switched around
 		// https://docs.wpilib.org/en/stable/docs/software/hardware-apis/motors/wpi-drive-classes.html
-		_differentialDrive.arcadeDrive(Subsystems.limitSpeed(-latSpeed),
-			Subsystems.limitSpeed(-longSpeed));
+		_differentialDrive.arcadeDrive(Subsystems.limitSpeed(latSpeed),
+			Subsystems.limitSpeed(longSpeed));
 	}
 
 	/**
